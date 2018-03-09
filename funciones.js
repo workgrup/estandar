313795ee2ba52e73606617e4f861ac1d595a7732
workgrup <camilobar20@hotmail.com>
2018-03-08 20:35:28 -0500

    Add files via upload

---
+++ funciones.js
@@ -0,0 +1,6 @@
+function raizc() {
+         x=Math.sqrt(x) //resolver raíz cuadrada.
+         pantalla.innerHTML=x; //mostrar en pantalla resultado
+         op="no"; //quitar operaciones pendientes.
+         xi=1;
+     }
